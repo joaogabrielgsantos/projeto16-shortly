9eab3beabd80481bb5f70cf758fcfc23f62b2caf
Joao Gabriel Gangorra dos Santos <jggsantos.25@gmail.com>
2022-10-16 07:06:24 -0300

    Configuracoes iniciais adicionadas

---
+++ src/db/database.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import pg from 'pg';
+
+const { Pool } = pg;
+
+dotenv.config();
+
+const user = 'postgres';
+const password = 'gangorra_1';
+const host = 'localhost';
+const port = 5432;
+const database = 'shortly';
+
+const connection = new Pool({
+  user,
+  password,
+  host,
+  port,
+  database
+});
+
+
+export default connection
