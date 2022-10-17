dc1fc5388d4e85003f95145c378c16b03f238035
Joao Gabriel Gangorra dos Santos <jggsantos.25@gmail.com>
2022-10-17 17:16:05 -0300

    Ajustes para deploy 3

--- src/db/database.js
+++ src/db/database.js
@@ -5,14 +5,14 @@ const { Pool } = pg;
 
 dotenv.config();
 
-
-
-const connection = new Pool({
+const databaseConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
-});
+}
+
+const connection = new Pool(databaseConfig);
 
 
 export default connection
