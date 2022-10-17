8e119af5ddd49325063bec49ffcdce9d78aaa9ea
Joao Gabriel Gangorra dos Santos <jggsantos.25@gmail.com>
2022-10-17 16:39:01 -0300

    Prepare for deploy

--- src/db/database.js
+++ src/db/database.js
@@ -5,20 +5,13 @@ const { Pool } = pg;
 
 dotenv.config();
 
-/* const user = 'postgres';
-const password = 'gangorra';
-const host = 'localhost';
-const port = 5432;
-const database = 'shortly'; */
 
-const connection = new Pool(/* {
-    user,
-    password,
-    host,
-    port,
-    database
-} */{
+
+const connection = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 
